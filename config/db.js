fab707475e0c8dc464d5e889a5c9c3697b9dd801
KarisNext <procompetentwriter@gmail.com>
2025-10-05 19:02:17 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -24,7 +24,7 @@ const createPool = () => {
       console.log('Using CPANEL Production Configuration...');
       config = {
         // Hardcoded, confirmed credentials for a stable connection
-        user: process.env.DB_USER || 'Karisdev2025',
+        user: process.env.DB_USER || 'vybeztribe_user',
         host: process.env.DB_HOST || '127.0.0.1', // CRITICAL: Use 127.0.0.1 for internal reliability
         database: process.env.DB_NAME || 'vybeztribe',
         password: process.env.DB_PASS || 'dere84ELIJOOH', // Your password
